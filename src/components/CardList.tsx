93bcd27a632fbf1175c7afb049016b9816efa3df
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-12-30 15:56:05 -0300

    quase pronto

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,28 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function openModal(url: string) {
+    onOpen();
+    setCurrentImageUrl(url);
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="10">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={openModal} />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={currentImageUrl}
+      />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,41 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          maxW="900px"
+          maxH="632px"
+          w="auto"
+          h="auto"
+          bgColor="pGray.800"
+        >
+          <ModalBody p={0}>
+            <Image src={imgUrl} maxW="900px" maxH="600px" />
+          </ModalBody>
+
+          <ModalFooter
+            bgColor="pGray.800"
+            h="8"
+            px="10px"
+            py="8px"
+            borderRadius="0px 0px 6px 6px"
+          >
+            <Link
+              href={imgUrl}
+              isExternal={true}
+              fontSize="sm"
+              lineHeight="4"
+              color="#F3F2F2"
+              mr="auto"
+            >
+              Abrir original
+            </Link>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
